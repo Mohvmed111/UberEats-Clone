94bf63e443f869887f24f05e7bc07d24feac9d84
Mohvmed111 <mohvmedzaki1@gmail.com>
2025-11-18 03:10:13 +0200

    upload changes

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -4,11 +4,12 @@ import { Home } from './home/home';
 import { Feed } from './pages/feed/feed';
 import { Restaurant } from './pages/restaurant/restaurant';
 import { Deliver } from './pages/deliver/deliver';
-
+import { Footer } from './footer/footer';
+import { ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,Home,Feed,Restaurant,Deliver],
+  imports: [RouterOutlet,Home,Feed,Restaurant,Deliver,Footer,ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
--- frontend/src/app/register/register.ts
+++ frontend/src/app/register/register.ts
@@ -1,32 +1,52 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth';
-import { RouterModule } from '@angular/router'; // ✅ لازم تضيف ده
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule,RouterModule],
+  imports: [FormsModule, RouterModule],
   templateUrl: './register.html',
   styleUrls: ['./register.css']
 })
 export class RegisterComponent {
-  user = { name: '', email: '', password: '' };
 
-  constructor(private authService: AuthService, private router: Router) {}
+  user = { 
+    name: '', 
+    email: '', 
+    password: '' 
+  };
+
+  confirmPassword: string = '';   // ⬅️ أضفناه
+  passwordMismatch: boolean = false; // ⬅️ للرسالة
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) {}
 
   onSubmit() {
-    this.authService.register(this.user).subscribe({
-      next: (res: any) => {
-        console.log('User registered:', res);
-        alert('Registration successful!');
-        this.router.navigate(['/login']);
-      },
-      error: (err) => {
-        console.error('Error:', err);
-        alert(err.error.message || 'Something went wrong!');
-      }
-    });
+
+  // Check match
+  if (this.user.password !== this.confirmPassword) {
+    this.passwordMismatch = true;
+    return;
   }
+
+  this.passwordMismatch = false;
+
+  // Submit request
+  this.authService.register(this.user).subscribe({
+    next: (res: any) => {
+      alert('Registration successful!');
+      this.router.navigate(['/login']);
+    },
+    error: (err) => {
+      alert(err.error.message || 'Something went wrong!');
+    }
+  });
+}
+
 }
